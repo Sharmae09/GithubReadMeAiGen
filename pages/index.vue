298ee9d150e0995b5d4fc2691f4d1ab3354315f8
Sharmae09 <sharmae.code@gmail.com>
2025-01-30 20:31:14 +0800

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+    <h1>Github Profile AI Generator</h1>
+    <h3>Let people know how amazing you are. Showcase your skill and project</h3>
+    <v-btn>Get Started</v-btn>
+    </div>
+</template>
+<script></script>
