964c91b467ccc91e87d6bca5d5975da941f75d72
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:44 +0800

    Update results.vue

--- pages/results.vue
+++ pages/results.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>Results</h1>
-  </div>
+  <v-sheet
+    class="d-flex justify-center align-center flex-column"
+    height="100vh"
+    width="100vw"
+    color="primary"
+  >
+    <div>
+      <h1>Results</h1>
+    </div>
+  </v-sheet>
 </template>
 <script></script>
