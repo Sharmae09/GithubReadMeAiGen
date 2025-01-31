06bd735fd906f7cd8e82a65590d43b1c98f11032
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:29 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import { NuxtPage } from "#components";
+</script>
+
 <template>
   <v-app>
+    <v-sheet
+      class="d-flex justify-center align-center flex-column"
+      height="100vh"
+      width="100vw"
+      color="primary"
+    >
+      <NuxtPage />
+    </v-sheet>
     <!-- .... -->
   </v-app>
 </template>
