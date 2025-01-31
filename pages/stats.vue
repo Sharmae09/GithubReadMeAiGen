933b1377479760e72cccec26c5ea8bbc4966013f
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:55 +0800

    Update stats.vue

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,6 +1,13 @@
 <template>
+  <v-sheet
+    class="d-flex justify-center align-center flex-column"
+    height="100vh"
+    width="100vw"
+    color="primary"
+  >
     <div>
-        <h1>Flex your github stats</h1>
+      <h1>Flex your github stats</h1>
     </div>
+  </v-sheet>
 </template>
 <script></script>
