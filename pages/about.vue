b8d4d0b1a278eae1e53299ea8a78f4f0f8a3855e
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:32 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <h1>ABOUT ME</h1>
-    <h2>Fil the Blanks</h2>
-  </div>
+  <v-sheet
+    class="d-flex justify-center align-center flex-column"
+    height="100vh"
+    width="100vw"
+    color="primary"
+  >
+    <div>
+      <h1>ABOUT ME</h1>
+      <h2>Fil the Blanks</h2>
+    </div>
+  </v-sheet>
 </template>
 <script></script>
