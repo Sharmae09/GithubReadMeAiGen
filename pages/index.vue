2dc3dcb5e4ccfe262417399de1ec0f5c44072caa
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:39 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
+  <v-sheet
+    class="d-flex justify-center align-center flex-column"
+    height="100vh"
+    width="100vw"
+    color="primary"
+  >
     <h1>Github Profile AI Generator</h1>
-    <h3>Let people know how amazing you are. Showcase your skill and project</h3>
+    <h3>
+      Let people know how amazing you are. Showcase your skill and project
+    </h3>
     <v-btn>Get Started</v-btn>
-    </div>
+  </v-sheet>
 </template>
 <script></script>
