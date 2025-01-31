ece23e20e196839734768d67d3a28fb5210b3d64
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:35 +0800

    Update donate.vue

--- pages/donate.vue
+++ pages/donate.vue
@@ -1,6 +1,13 @@
 <template>
+  <v-sheet
+    class="d-flex justify-center align-center flex-column"
+    height="100vh"
+    width="100vw"
+    color="primary"
+  >
     <div>
-        <h1>Donate</h1>
+      <h1>Donate</h1>
     </div>
+  </v-sheet>
 </template>
 <script></script>
