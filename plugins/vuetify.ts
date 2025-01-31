2bb1e2dcb77152c4abe78bf850ba8f0688953e14
Sharmae09 <sharmae.code@gmail.com>
2025-01-31 14:01:59 +0800

    Update vuetify.ts

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -6,7 +6,28 @@ import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    theme: {
+      defaultTheme: "dark",
+      themes: {
+        dark: {
+          colors: {
+            primary: "#11052C",
+            secondary: "#3D087B",
+            tertiary: "#090088",
+            accent: "#F43B86",
+            four: "#EEEEEE",
+            five: "#FFBD39",
+          },
+        },
+      },
+    },
+    defaults: {
+      global: {
+        style: {
+          fontFamily: "Roboto, sans-serif",
+        },
+      },
+    },
   });
   app.vueApp.use(vuetify);
 });
