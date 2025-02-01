1af3feadbb419cf581c91730797e05f2768b8341
Sharmae09 <sharmae.code@gmail.com>
2025-02-01 18:59:38 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
+  css: ["@/assets/styles/settings.scss"],
+  // head: {
+  //   link: [
+  //     {
+  //       rel: "stylesheet",
+  //       href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap",
+  //     },
+  //   ],
+  // },
   compatibilityDate: "2024-11-01",
   //...
   build: {
