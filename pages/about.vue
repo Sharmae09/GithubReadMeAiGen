a841ec9ac76dbe98404e38479bed42398d172da3
Sharmae09 <sharmae.code@gmail.com>
2025-02-01 18:59:29 +0800

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -5,10 +5,37 @@
     width="100vw"
     color="primary"
   >
+    <h1 class="text-center" style="color: #fec5fb">ABOUT ME</h1>
+
     <div>
-      <h1>ABOUT ME</h1>
       <h2>Fil the Blanks</h2>
+      <v-text-field
+        class="custom-size"
+        v-model="firstname"
+        :label="disabled"
+        required
+        variant="outlined"
+      ></v-text-field>
+      <v-btn @click="Prev">Prev</v-btn>
+      <v-btn @click="Next">Next</v-btn>
     </div>
   </v-sheet>
 </template>
-<script></script>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const Next = () => {
+  router.push("/skills");
+};
+const Prev = () => {
+  router.push("/");
+};
+</script>
+<style scoped>
+.custom-size {
+  width: 50vw;
+  height: 60vh; /* Adjust height here */
+}
+</style>
