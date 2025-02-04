5546631b5b809afe8db716b7bd2967295c37374b
Sharmae09 <sharmae.code@gmail.com>
2025-02-04 23:05:22 +0800

    Create markdownV.vue

---
+++ components/markdownV.vue
@@ -0,0 +1,161 @@
+<template>
+  <div v-html="markdownToHtml(markdownText)"></div>
+</template>
+
+<script setup>
+import { useNuxtApp } from "#app";
+const { $showdown } = useNuxtApp();
+
+const username = useState("username", () => "");
+const aboutme = useState("aboutme", () => "");
+const techstack = useState("selectedSkills", () => []);
+const socialUsernames = useState("socialUsernames", () => [
+  {
+    media: "Behance",
+    username: "your-behance",
+    icon: "mdi-behance",
+    link: "https://www.behance.net/your-behance",
+  },
+  {
+    media: "Medium",
+    username: "your-medium",
+    icon: "mdi-medium",
+    link: "https://medium.com/@your-medium",
+  },
+  {
+    media: "Quora",
+    username: "your-quora",
+    icon: "mdi-quora",
+    link: "https://www.quora.com/profile/your-quora",
+  },
+  {
+    media: "Facebook",
+    username: "your-facebook",
+    icon: "mdi-facebook",
+    link: "https://www.facebook.com/your-facebook",
+  },
+  {
+    media: "Twitter",
+    username: "your-twitter",
+    icon: "mdi-twitter",
+    link: "https://twitter.com/your-twitter",
+  },
+  {
+    media: "Instagram",
+    username: "your-instagram",
+    icon: "mdi-instagram",
+    link: "https://instagram.com/your-instagram",
+  },
+  {
+    media: "LinkedIn",
+    username: "your-linkedin",
+    icon: "mdi-linkedin",
+    link: "https://linkedin.com/in/your-linkedin",
+  },
+  {
+    media: "GitHub",
+    username: "your-github",
+    icon: "mdi-github",
+    link: "https://github.com/your-github",
+  },
+  {
+    media: "YouTube",
+    username: "your-youtube",
+    icon: "mdi-youtube",
+    link: "https://youtube.com/c/your-youtube",
+  },
+  {
+    media: "TikTok",
+    username: "your-tiktok",
+    icon: "mdi-tiktok",
+    link: "https://tiktok.com/@your-tiktok",
+  },
+  {
+    media: "Reddit",
+    username: "your-reddit",
+    icon: "mdi-reddit",
+    link: "https://www.reddit.com/user/your-reddit",
+  },
+  {
+    media: "Stackoverflow",
+    username: "your-stackoverflow",
+    icon: "mdi-stackoverflow",
+    link: "https://stackoverflow.com/users/your-stackoverflow",
+  },
+  {
+    media: "Twitch",
+    username: "your-twitch",
+    icon: "mdi-twitch",
+    link: "https://www.twitch.tv/your-twitch",
+  },
+  {
+    media: "Discord",
+    username: "your-discord",
+    icon: "mdi-discord",
+    link: "https://discord.com/your-discord",
+  },
+]);
+
+// Generate Markdown String
+const socialLinksMarkdown = computed(() =>
+  socialUsernames.value
+    .filter((user) => user.username) // Only include accounts with a username
+    .map((user) => `- [${user.media}]( ${user.link} )`)
+    .join("\n")
+);
+
+console.log(username);
+console.log("username.value");
+
+const markdownText = `#  Hi there! 👋 I'm ${username.value}
+${aboutme.value}
+
+---
+
+## 🚀 Technologies & Tools
+
+${techstack.value}
+
+---
+
+## 🎯 GitHub Stats
+![](https://github-readme-streak-stats.herokuapp.com/?user=${username.value}&&theme=radical&hide_border=false)<br/>
+![top lang](https://github-readme-stats.vercel.app/api/top-langs/?username=${username.value}&layout=compact&theme=radical)<br>
+
+
+---
+
+## 🌟 Projects
+
+- [**AI Resume and Cover Letter Generator**](https://github.com/yourusername/project1): A brief description of what the project does.  
+- [**API Marketplace**](https://github.com/yourusername/project2): Another project with a short description.  
+- [**Dental Appointment App**](https://github.com/yourusername/project2): Another project with a short description.  
+
+---
+
+## 🌐 Get in Touch
+${socialLinksMarkdown.value}
+- 💼 Portfolio: https://sharmaethegreat.com/
+- 📧 Email: [sharmae.code@email.com](mailto:sharmae.code@email.com)
+
+--- 
+
+### ✍️ Quote of the day
+![](https://quotes-github-readme.vercel.app/api?type=horizontal&theme=radical)
+
+--- 
+## 💰 Support My Work
+-  [☕ Buy Me a Coffee](https://buymeacoffee.com/sharmae)
+  
+---
+
+
+
+<!-- Proudly created with -->
+`;
+
+// const markdownText = `# Welcome to My README Generator
+//   This is powered by **Showdown**!`;
+
+const markdownToHtml = (md) => $showdown.makeHtml(md);
+</script>
